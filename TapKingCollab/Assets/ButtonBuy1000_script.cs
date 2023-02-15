8ff9e6331a8047ebd6afabb961c3b0f9501147b5
Gototop23 <merijndebrouwer@gmail.com>
2023-02-16 00:10:52 +0100

    1

--- TapKingCollab/Assets/ButtonBuy1000_script.cs
+++ TapKingCollab/Assets/ButtonBuy1000_script.cs
@@ -8,23 +8,44 @@ public class ButtonBuy1000_script : MonoBehaviour
 {
     public Enemy_Script CurrencyAmount;
 
+    public int total;
+    public int randomNumber;
+
+    public item1_Script item1;
+
+
     void Start()
     {
         gameObject.GetComponent<Button>().onClick.AddListener(BuyItem1000);
+
+
     }
 
     void BuyItem1000()
     {
         CurrencyAmount = FindObjectOfType<Enemy_Script>();
+        item1 = FindObjectOfType<item1_Script>();
+
+        
 
         if (CurrencyAmount.Currency >= 1000);
         {
             CurrencyAmount.Currency -= 1000;
             Debug.Log ("bought Gear");
 
+            randomNumber = Random.Range(0, 200);
+
+            if (randomNumber > 20 && randomNumber < 50)
+            {
+                    Debug.Log ("award giant pointer"); 
+                    item1.Amount += 1;        
+            }
+
+            }
+
         }
+
     
 }
 
-}
 
